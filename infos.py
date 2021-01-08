1524ea2b9d224ae9127873a1b5f6c191a19405d3
Casey <caseyprimeau@gmail.com>
2021-01-07 19:19:19 -0800

    gitignore update

---
+++ infos.py
@@ -0,0 +1,37 @@
+analytics_string = '''<!DOCTYPE html>
+<html>
+<head>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-TRC3906SWR"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-TRC3906SWR');
+    </script>
+
+<!-- End Global Google Analytics -->
+{%metas%}
+<title>{%title%}</title>
+{%favicon%}
+{%css%}
+</head>
+<body>
+{%app_entry%}
+<footer>
+{%config%}
+{%scripts%}
+{%renderer%}
+</footer>
+</body>
+</html>
+'''
+
+
+
+###********************* dash ***************************************
+VALID_USERNAME_PASSWORD_PAIRS = {
+    'hello': 'woild',
+    'guest':'guest'
+}
